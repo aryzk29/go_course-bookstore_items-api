c423e173bd37c49e2694ad20c6226099a9d57fd3
aryar <arya.rizky4@gmail.com>
2022-09-15 15:10:47 +0700

    add item domain

---
+++ domain/item.go
@@ -0,0 +1,24 @@
+package domain
+
+type Item struct {
+	Id                string      `json:"id"`
+	Seller            int64       `json:"seller"`
+	Title             string      `json:"title"`
+	Description       Description `json:"description"`
+	Pictures          []Picture   `json:"pictures"`
+	Video             string      `json:"video"`
+	AvailableQuantity int         `json:"available_quantity"`
+	SoldQuantity      int         `json:"sold_quantity"`
+	Price             float32     `json:"price"`
+	Status            string      `json:"status"`
+}
+
+type Description struct {
+	PlainText string `json:"plain_text"`
+	Html      string `json:"html"`
+}
+
+type Picture struct {
+	id  int64  `json:"id"`
+	url string `json:"url"`
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aryzk29/go_course-bookstore_items-api
+
+go 1.18
